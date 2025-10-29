ca6d79b93d89ededf7311f535377bd95a880a0c1
Martiņš <A240677MZ@rvt.lv>
2025-10-29 13:05:48 +0200

    DivisableByThree

---
+++ src/main/java/rvt/DivisableByThree.java
@@ -0,0 +1,14 @@
+package rvt;
+
+public class DivisableByThree {
+    public static void main(String[] args) {
+        divisibleByThreeInRange(2, 10);
+    }
+    public static void divisibleByThreeInRange(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
