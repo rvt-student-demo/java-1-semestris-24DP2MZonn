6a1bdabdd1df91d754fbf9baf8e17c0c9c3e3ab7
Martiņš <A240677MZ@rvt.lv>
2025-10-31 13:16:53 +0200

    Advanced astrology part 3

--- src/main/java/rvt/AdvanceAstrology.java
+++ src/main/java/rvt/AdvanceAstrology.java
@@ -13,13 +13,17 @@ public class AdvanceAstrology {
         System.out.println();
         
     }
-    public static void printTriangle(int size) {
-        for (int i = 1; i <= size ; i++){
-            printSpaces(size - i);
-            printStars(i);
+    public static void christmasTree(int height) {
+        for (int i = 1; i <= height ; i++){
+            printSpaces(height - i);
+            printStars(2 * i - 1);
         }
+        printSpaces(height - 2);
+        printStars(3);
+        printSpaces(height - 2);
+        printStars(3);
     }
     public static void main(String[] args) {
-        printTriangle(4);
+        christmasTree(7);
     }
 }
